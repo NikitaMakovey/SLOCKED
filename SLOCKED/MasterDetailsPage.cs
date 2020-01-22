3a525e6d7506c34f08283984efb33e7f3693409c
NikitaMakovey <niks.finn@gmail.com>
2020-01-22 21:31:53 +1000

    Created basic namespace configs and xaml files for working

---
+++ SLOCKED/MasterDetailsPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SLOCKED
+{
+    public class MasterDetailsPage : ContentPage
+    {
+        public MasterDetailsPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ SLOCKED/MasterDetailsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SLOCKED
+{
+    public partial class MasterDetailsPage : MasterDetailPage
+    {
+        public MasterDetailsPage()
+        {
+            InitializeComponent();
+
+            masterPage.listView.ItemSelected += OnItemSelected;
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                MasterBehavior = MasterBehavior.Popover;
+            }
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
